e96e4e5f69482a22c24606033723122ef65442f8
Jim <jamestprime02@icloud.com>
2021-03-09 12:39:52 +0200

    Add files via upload

---
+++ src/api/yelp.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+
+
+export default axios.create({
+   baseURL:  'https://api.yelp.com/v3/businesses',
+    headers: {
+        Authorization: 'Bearer your_token'
+    }
+});
+
+
+
+
+
